c16856bde536f4975ad4cd2371a945cc0c37f6a2
Curtis Oliver <cuoliver@syr.edu>
2019-04-04 15:21:03 -0400

    Replaced mysql commands with msSQL. Added readme for localhost instructions

--- Web Portal/connect.php
+++ Web Portal/connect.php
@@ -1,14 +1,28 @@
 <?php
-//Info for DB connection. Replace with real credentials once we have it setup
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname = "test";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
+
+
+$serverName = "haiproject.database.windows.net";
+
+$connectionOptions = array(
+
+        "Database" => "Inventory",
+
+        "Uid" => "haiadmin",
+
+        "PWD" => "HAIpassw0rd"
+
+                );
+
+
+
+$conn = sqlsrv_connect($serverName, $connectionOptions) or DIE("Failed to connect to database");
+
+
+
+if ($conn)->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
 ?>
