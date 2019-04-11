928c675ff17abe74717e3cc34d5849ebbd9963c0
nlrobson <43036854+nlrobson@users.noreply.github.com>
2019-04-11 17:52:24 -0400

    CSV Parser - Nick

--- src/local/csvParser.java
+++ src/local/csvParser.java
@@ -2,49 +2,99 @@ package local;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import org.apache.commons.csv.*;
+import items.item;
 
 public class csvParser {
 
-        public List<List<String>> readFile() throws Exception {
+        public List<Item> readFile() throws Exception {
+
+            //Item item = new Item();
+            List<Item>  itemList = new ArrayList<>();
+
             String fileName= "src\\local\\useritems.csv";
             File file= new File(fileName);
             String path = file.getAbsolutePath();
+            Reader in = new FileReader(path);
+            Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader("itemNo","room","category","type","make","model","serial","receipt","photo","value","comments","lastupdate","delete").parse(in);
+            for (CSVRecord record : records) {
 
-            System.out.println("File Path " + path);
-            // this gives you a 2-dimensional array of strings
-            List<List<String>> lines = new ArrayList<>();
-            Scanner inputStream;
+               //Item thing = new Item(record.get("itemNo"),record.get("category"),record.get("type"),record.get("make"),record.get("model"),record.get("serial"),record.get("receipt"),record.get("photo"),record.get("value"),record.get("comments"));
 
-            try{
-                inputStream = new Scanner(file);
-
-                while(inputStream.hasNext()){
-                    String line= inputStream.next();
-                    String[] values = line.split(",");
-                    // this adds the currently parsed line to the 2-dimensional string array
-                    lines.add(Arrays.asList(values));
-                }
-
-                inputStream.close();
-            }catch (Exception e) {
-                e.printStackTrace();
+                String itemNo = record.get("itemNo");
+                String room = record.get("room");
+                String category = record.get("category");
+                String type = record.get("type");
+                String make = record.get("make");
+                String model = record.get("model");
+                String serial = record.get("serial");
+                String receipt = record.get("receipt");
+                String photo = record.get("photo");
+                String value = record.get("value");
+                String comments = record.get("comments");
+                Item item = new Item(itemNo, room, category, type, make, model, serial, receipt, photo, value, comments);
+                itemList.add(item);
+                //System.out.println(itemNo);
             }
 
-            // the following code lets you iterate through the 2-dimensional array
-            //int lineNo = 1;
-            //for(List<String> line: lines) {
-            //    int columnNo = 1;
-            //    for (String value: line) {
-            //        System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
-            //        columnNo++;
-            //    }
-            //    lineNo++;
-            //}
-            return lines;
+            return itemList;
         }
 
+
+
+    public class Item {
+        //private boolean delete;
+        private String itemNo;
+        private String room;
+        private String category;
+        private String type;
+        private String make;
+        private String model;
+        private String serial;
+        private String receipt;
+        private String photo;
+        private String value;
+        private String comments;
+
+        public Item() {}
+
+        public Item(String item, String room, String cat, String type, String make, String model, String serial, String receipt, String photo, String value, String comment) {
+            //this.delete = del;
+            this.itemNo = item;
+            this.room = room;
+            this.category = cat;
+            this.type = type;
+            this.make = make;
+            this.model = model;
+            this.serial = serial;
+            this.receipt = receipt;
+            this.photo = photo;
+            this.value = value;
+            this.comments = comment;
+        }
+
+        // public boolean getDelete() { return delete;}
+        public String getItemNo() { return itemNo;}
+        public String getRoom() { return room;}
+        public String getCategory() { return category;}
+        public String getType() { return type;}
+        public String getMake() { return make;}
+        public String getModel() { return model;}
+        public String getSerial() { return serial;}
+        public String getReceipt() { return receipt;}
+        public String getPhoto() { return photo;}
+        public String getValue() { return value;}
+        public String getComments() { return comments;}
+
+        //public void setDelete(boolean b) { delete = b; }
+
+    }
+
 }
+
