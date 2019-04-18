6ca79c9db4fd27331d5ba42eddc232349d9d7eda
Meagan Sanchez <mnsanche@syr.edu>
2019-04-18 12:39:24 -0400

    Login class

--- src/items/Item.java
+++ src/items/Item.java
@@ -45,57 +45,49 @@ public class Item {
 
         if (room == null) {
             this.room = oldItem.getRoom();
-        }
-        else {
+        } else {
             this.room = room;
         }
 
         if (category == null) {
             this.category = oldItem.getCategory();
-        }
-        else {
+        } else {
             this.category = category;
         }
 
         if (type == null) {
             this.type = oldItem.getType();
-        }
-        else {
+        } else {
             this.type = type;
         }
 
         if (make.equals("")) {
             this.make = oldItem.getMake();
-        }
-        else {
+        } else {
             this.make = make;
         }
 
         if (model.equals("")) {
             this.model = oldItem.getModel();
-        }
-        else {
+        } else {
             this.model = model;
         }
 
         if (serial.equals("")) {
             this.serial = oldItem.getSerial();
-        }
-        else {
+        } else {
             this.serial = serial;
         }
 
         if (receipt.equals("")) {
             this.receipt = oldItem.getReceipt();
-        }
-        else {
+        } else {
             this.receipt = receipt;
         }
 
         if (photo.equals("")) {
             this.photo = oldItem.getPhoto();
-        }
-        else {
+        } else {
             this.photo = photo;
         }
 
@@ -104,17 +96,12 @@ public class Item {
 
         if (comments.equals("")) {
             this.comments = oldItem.getComments();
-        }
-        else {
+        } else {
             this.comments = comments;
         }
         //
 
     }
-
-
-
-
 
 
     //Delete an Item
@@ -132,7 +119,7 @@ public class Item {
     }*/
 
     public Room getRoom() {
-    return room;
+        return room;
     }
 
     public Category getCategory() {
@@ -175,7 +162,36 @@ public class Item {
     public boolean Compare(Item two) {
         Float f1 = new Float(this.getValue());
         Float f2 = new Float(two.getValue());
-        boolean equals = this.getRoom().equals(two.getRoom()) && this.getCategory().equals(two.getCategory()) && this.getType().equals(two.getType()) && this.getMake().equals(two.getMake()) && this.getModel().equals(two.getModel()) && this.getSerial().equals(two.getSerial()) && this.getReceipt().equals(two.getReceipt()) && this.getPhoto().equals(two.getPhoto()) && Float.compare(f1, f2) == 0 && this.getComments().equals(two.getComments());
-        return equals;
+        if (!this.getRoom().equals(two.getRoom())) {
+            return false;
+        }
+        if (!this.getCategory().equals(two.getCategory())) {
+            return false;
+        }
+        if (!this.getType().equals(two.getType())) {
+            return false;
+        }
+        if (!this.getMake().equals(two.getMake())) {
+            return false;
+        }
+        if (!this.getModel().equals(two.getModel())) {
+            return false;
+        }
+        if (!this.getSerial().equals(two.getSerial())) {
+            return false;
+        }
+        if (!this.getReceipt().equals(two.getReceipt())) {
+            return false;
+        }
+        if (!this.getPhoto().equals(two.getPhoto())) {
+            return false;
+        }
+        if (Float.compare(f1, f2) != 0) {
+            return false;
+        }
+        if (!this.getComments().equals(two.getComments())) {
+            return false;
+        }
+        return true;
     }
 }
