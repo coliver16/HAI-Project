41ffe1002eef79d8e6b030863de684ee1e8d3342
nlrobson <43036854+nlrobson@users.noreply.github.com>
2019-04-18 13:16:27 -0400

    Adjusting how Items work.

---
+++ src/items/ItemList.java
@@ -0,0 +1,46 @@
+package items;
+
+import com.google.common.eventbus.Subscribe;
+import local.ParseEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemList {
+
+    public class EventHandler {
+        @Subscribe
+        public void parseEvent(ParseEvent event) {
+            System.out.println("Made it to event");
+            itemList = (List) event.getMessage();
+            //System.out.println("Event: " + event.toString());
+            //System.out.println(itemImports.get(0).make);
+            //for (Object i : itemImports) {
+             //   itemList.getItems().add(i);
+            //}
+        }
+    }
+
+    public static List<Item> itemList = new ArrayList<Item>();
+
+    public static void setItemList(List<Item> i) {
+        itemList = i;
+    }
+
+
+    public static List<Item> getItemList() {
+        return itemList;
+    }
+
+    public List<Item> addItem(Item i) {
+        itemList.add(i);
+        return itemList;
+    }
+
+    public Item deleteItem(Item i){
+        int j = itemList.indexOf(i);
+        itemList.get(j).deleted = true;
+        return itemList.get(j);
+    }
+
+}
