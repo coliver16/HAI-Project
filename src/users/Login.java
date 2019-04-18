fa2e9f59a90a915fe3837b5dc9acf8bdcd307c08
Meagan Sanchez <mnsanche@syr.edu>
2019-04-18 14:59:57 -0400

    Login

--- src/users/Login.java
+++ src/users/Login.java
@@ -59,6 +59,7 @@ public class Login {
                 }
                 catch (SQLException e) {
                     e.printStackTrace();
+                    //
                 }
             }
         }
