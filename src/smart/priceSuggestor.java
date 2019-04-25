7c4313693dabe74a87d6f44466bd6a070d6b35a1
Meagan Sanchez <mnsanche@syr.edu>
2019-04-25 17:57:50 -0400

    Deviate Class + TODOs

--- src/smart/priceSuggestor.java
+++ src/smart/priceSuggestor.java
@@ -26,11 +26,12 @@ public class priceSuggestor {
         }
     }
 
+    //TODO: Try changing to match comments instead as well (search for year in comments)
     public float Suggest(String serial) {
         float estimate = 0;
         int medindex;
         int size = 0;
-        dev = deviates();
+        //TODO: Fix this line to add dev - dev = deviates();
         //Create an array to be able to select prices by index
         Float[] priceArray = new Float[size];
         //Create an array list to be able to add prices dynamically
@@ -50,55 +51,51 @@ public class priceSuggestor {
             //Sort the array in asceding order
             Collections.sort(priceList);
             //if the size of the array is odd, the median is the middle number
-            if (size/2 != 0) {
-                medindex = (size+1)/2;
+            if (size / 2 != 0) {
+                medindex = (size + 1) / 2;
                 estimate = priceArray[medindex];
             }
             //if the size of the array is even, the median is the average of the two middle numbers
             else {
-                medindex = size/2;
-                estimate = (priceArray[medindex]+priceArray[medindex+1])/2;
+                medindex = size / 2;
+                estimate = (priceArray[medindex] + priceArray[medindex + 1]) / 2;
             }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 if (rs != null) rs.close();
-            }
-            catch (SQLException e) {
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
             try {
                 if (pstmt != null) pstmt.close();
-            }
-            catch (SQLException e) {
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
             try {
                 if (conn != null) conn.close();
-            }
-            catch (SQLException e) {
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
         //Account for deviation
-        estimate += estimate*dev;
+        //TODO: Fix this line to add dev - estimate += estimate * dev;
         return estimate;
     }
 
+    //TODO: Search for year in comments and add that to be passed
     public float Suggest(String make, String model) {
         float estimate = 0;
         int medindex;
         int size = 0;
-        dev = deviates();
+        // TODO: FIX this line: dev = deviates();
         Float[] priceArray = new Float[size];
         ArrayList<Float> priceList = new ArrayList<>();
         try {
             pstmt = conn.prepareStatement("select item_price from items_454 where item_make = ? and item_model = ?");
             pstmt.setString(1, make);
-            pstmt.setString(1, model);
+            pstmt.setString(2, model);
             rs = pstmt.executeQuery();
             while (rs.next()) {
                 priceList.add(rs.getFloat("item_price"));
@@ -106,19 +103,16 @@ public class priceSuggestor {
             }
             priceArray = priceList.toArray(priceArray);
             Collections.sort(priceList);
-            if (size/2 != 0) {
-                medindex = (size+1)/2;
+            if (size / 2 != 0) {
+                medindex = (size + 1) / 2;
                 estimate = priceArray[medindex];
+            } else {
+                medindex = size / 2;
+                estimate = (priceArray[medindex] + priceArray[medindex + 1]) / 2;
             }
-            else {
-            medindex = size/2;
-            estimate = (priceArray[medindex]+priceArray[medindex+1])/2;
-            }
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 if (rs != null) rs.close();
             } catch (SQLException e) {
@@ -136,17 +130,13 @@ public class priceSuggestor {
             }
         }
         //Account for deviation
-        estimate += estimate*dev;
+        //TODO: Fix this line to add dev - estimate += estimate *;
         return estimate;
-    }
-
-    public float deviates() {
-        //Store suggested prices and deviating prices until we get up to 10 devaiting prices
-        //Calculate the deviation from the suggested for each using the equation (Deviating Price - Suggested Price)/Suggested Price
-        //Find the mean of all those deviations excluding those means that are higher than .8 or lower than -.8 (in order to weed out bad input)
-        //Return that mean (variable dev)
-        //Every 10 added deviation prices, we repeat this process automatically tp update dev
-        return dev;
     }
 }
 
+    //Store suggested prices and deviating prices until we get up to 10 devaiting prices
+    //Calculate the deviation from the suggested for each using the equation (Deviating Price - Suggested Price)/Suggested Price
+    //Find the mean of all those deviations excluding those means that are higher than .8 or lower than -.8 (in order to weed out bad input)
+    //Return that mean (variable dev)
+    //Every 10 added deviation prices, we repeat this process automatically tp update dev
